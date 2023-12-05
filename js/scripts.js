43af3e6a32d316463d4743817d2de57c4fb82bd7
JordanRomagnoli <jordanromagnoli1999@gmail.com>
2023-12-05 18:46:48 +0100

    - SETT variables - CREATE html elements from js - ADD class from js - STAMP in html - ADD style

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,39 @@
-for( let i = 1;  i <= 100; i++ ){
+    let i;
+    const container = document.getElementById('elements');
+    
+    
 
+for( i = 1;  i <= 100; i++ ){
+
+    const square = document.createElement('div');
+    square.classList.add('col-2','p-4','text-center','rounded-4','m-2','text-white','border','border-white')
+    
     
 	if ((i % 3 == 0) && (i % 5 == 0)){
-        console.log ("fizz" + "-" + "buzz")
-    }else if (i % 3 == 0){
-        console.log ("fizz")
-    }else if (i % 5 == 0){
-        console.log ("buzz")
-    }else {
-        console.log(i);
+        square.classList.add('bg-warning');
+        square.append("FizzBuzz")
     }
     
     
+    else if (i % 3 == 0){
+        square.classList.add('bg-danger');
+        square.append("fizz");
+    }
+    
+    
+    else if (i % 5 == 0){
+        square.classList.add('bg-primary');
+        square.append("buzz");
+    }
+    
+    
+    else {
+        square.classList.add('bg-secondary');
+        square.append(i);
+        
+    }
+    
+    container.append(square)
     
 }
+
